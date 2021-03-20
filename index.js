dfff289ff566592df82eb5da23603640ae7a724d
Relysia <baranyairiki@gmail.com>
2021-03-20 13:10:47 +0100

    Cors Now Enabled Please

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
 // middlewarre
+app.use(cors({ credentials: true }));
 app.use(express.json());
-// app.use(cors({ origin: "*" }));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 const fs = require("fs").promises;
 app.get("/api/todo", async (req, res) => {
