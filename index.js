08fec191dfc170b424159205e081b4d23970af53
Relysia <baranyairiki@gmail.com>
2021-03-20 12:50:02 +0100

    Cors update v1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 // middlewarre
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
